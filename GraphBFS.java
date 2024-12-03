d8765f1952fe6de7536030812afe44e0c1362f85
Carol Balleza <carol.balleza05@gmail.com>
2024-12-02 23:35:40 -0800

    created commits for GraphBFS

--- GraphBFS.java
+++ GraphBFS.java
@@ -1,37 +1,51 @@
 import java.util.LinkedList;
 import java.util.Queue;
+
 public class GraphBFS {
-    private int vertices;
-    private LinkedList<Integer> adjList[];
-    private char nodes[];
+    private int vertices; //number of vertices in graph
+    private LinkedList<Integer> adjList[]; //adjacency list representation of graph
+    private char nodes[]; //labels of nodes
+
+//constructor to initialize graph
 public GraphBFS(int v){
     vertices = v;
     adjList = new LinkedList[v];
     nodes = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
+
+    //initialize adjacency lists for all vertices
     for(int i = 0; i < v; i++){
         adjList[i] = new LinkedList<>();
     }
 }
+
+//method to add edge to graph
    public void addEdge(char v, int e){
-      int vIndex = v - 'A';
-      int eIndex = e - 'A';
-      adjList[vIndex].add(eIndex);
+      int vIndex = v - 'A'; //convert char to index
+      int eIndex = e - 'A'; //covert char to index
+      adjList[vIndex].add(eIndex); //add edge to adjacency list
     }
+
+    //method to perform BFS traversal starting from given vertex
     public void BFS(char startVertexchar){
-        int startVertex = startVertexchar - 'A';
-        boolean visited[] = new boolean[vertices];
-        Queue<Integer> queue = new LinkedList<>();
-        visited[startVertex] = true;
-        queue.add(startVertex);
+        int startVertex = startVertexchar - 'A'; //convert start vertex char to index
+        boolean visited[] = new boolean[vertices]; //array to keep track of visited vertices
+        Queue<Integer> queue = new LinkedList<>(); //queue for BFS
+
+        visited[startVertex] = true; //mark start vertex as visited
+        queue.add(startVertex); //add start vertex to queue
+
+        //BFS loop
         while(!queue.isEmpty()){
-            int vertex = queue.remove();
-            System.out.print(nodes[vertex] + " ");
+            int vertex = queue.remove(); //dequeue vertex from queue
+            System.out.print(nodes[vertex] + " "); //print visited vertex
+
+            //gets all adjacent vertices of dequeued vertex
             int size = adjList[vertex].size();
             for(int i = 0; i < size; i++){
                 int n = adjList[vertex].get(i);
-                if(!visited[n]){
-                    visited[n] = true;
-                    queue.add(n);
+                if(!visited[n]){ //if adjacent vertex has not been visited
+                    visited[n] = true; //mark as visited
+                    queue.add(n); //enqueue it 
                 }
             }
         }
